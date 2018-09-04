3c2e838a4d90c7f494ccaf614a1c7f4032bef6fe
Astrid Varga <astrid.varga@gmx.de>
2018-09-04 17:38:06 +0200

    Translator profile edit: show price field if professional translator is logged in and for others only by checked radio button

--- routes/profile.js
+++ routes/profile.js
@@ -6,50 +6,50 @@ const WO = require("../models/WO");
 
 //use this middleware at every request to check if user is logged in
 router.use((req, res, next) => {
-  //if there is not a running session with a user
-  if (!req.user) {
-    res.render("index", {
-      message: "You must be logged in to view this page"
-    });
-  } else {
-    next();
-  }
+    //if there is not a running session with a user
+    if (!req.user) {
+        res.render("index", {
+            message: "You must be logged in to view this page"
+        });
+    } else {
+        next();
+    }
 });
 
 router.get("/show", (req, res) => {
-  const { id } = req.user;
-  if (req.user.role === "WO") {
-    WO.findOne({ user: id })
-      .populate("user")
-      .then(wo => {
-        res.render("profile", { wo });
-      })
-      .catch(console.error);
-  } else if (req.user.role === "Translator") {
-    Translator.findOne({ user: id }).then(translator => {
-      res.render("profile", { translator });
-    });
-  }
+    const { id } = req.user;
+    if (req.user.role === "WO") {
+        WO.findOne({ user: id })
+            .populate("user")
+            .then(wo => {
+                res.render("profile", { wo });
+            })
+            .catch(console.error);
+    } else if (req.user.role === "Translator") {
+        Translator.findOne({ user: id }).then(translator => {
+            res.render("profile", { translator });
+        });
+    }
 });
 
 router.get("/edit", (req, res) => {
-  const { id } = req.user;
+    const { id } = req.user;
 
-  if (req.user.role === "WO") {
-    WO.findOne({ user: id })
-      .populate("user")
-      .then(wo => {
-        res.render("edit", { wo });
-      })
-      .catch(console.error);
-  } else if (req.user.role === "Translator") {
-    Translator.findOne({ user: id }).then(translator => {
-      res.render("edit", { translator });
-    });
-  }
+    if (req.user.role === "WO") {
+        WO.findOne({ user: id })
+            .populate("user")
+            .then(wo => {
+                res.render("edit", { wo });
+            })
+            .catch(console.error);
+    } else if (req.user.role === "Translator") {
+        Translator.findOne({ user: id }).then(translator => {
+            res.render("edit", { translator, isProfessional: translator.role === "Professional" });
+        });
+    }
 });
 router.post("/edit", (req, res) => {
-  //TODO implement overwriting of db data
+    //TODO implement overwriting of db data
 });
 
 module.exports = router;
