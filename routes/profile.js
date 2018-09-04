88305ee6f63ea809f145c63357d7b2ea22e6b967
adiyathereh <ad.kydy@gmail.com>
2018-09-04 13:21:20 +0200

    fixed login-logout issue

--- routes/profile.js
+++ routes/profile.js
@@ -5,21 +5,27 @@ const Translator = require("../models/Translator");
 const WO = require("../models/WO");
 
 router.get("/show", (req, res) => {
-  const { id } = req.user;
-  if (req.user.role === "WO") {
-    WO.findOne({ user: id })
-      .populate("user")
-      .then(wo => {
-        res.render("profile", { wo });
-      })
-      .catch(console.error);
-  } else if (req.user.role === "Translator") {
-    Translator.findOne({ user: id })
-      .populate("user")
-      .then(translator => {
-        res.render("profile", { translator });
-      })
-      .catch(console.error);
+  if (!req.user) {
+    res.render("index", {
+      message: "You must be logged in to view this page"
+    });
+  } else {
+    const { id } = req.user;
+    if (req.user.role === "WO") {
+      WO.findOne({ user: id })
+        .populate("user")
+        .then(wo => {
+          res.render("profile", { wo });
+        })
+        .catch(console.error);
+    } else if (req.user.role === "Translator") {
+      Translator.findOne({ user: id })
+        .populate("user")
+        .then(translator => {
+          res.render("profile", { translator });
+        })
+        .catch(console.error);
+    }
   }
 });
 
