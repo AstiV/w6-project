be0830f33e377e1e951e1e752b67dcedb0eb0582
adiyathereh <ad.kydy@gmail.com>
2018-09-05 11:17:39 +0200

    languages are formatted now

--- utils/editFormFormatting.js
+++ utils/editFormFormatting.js
@@ -8,17 +8,35 @@ module.exports = function formatFields(rawData) {
       if (field === "username" || field === "email") {
         userModelFields[field] = rawData[field].trim();
       } else if (field === "price" && rawData[field][0] === "") {
+      } else if (field === "language" || field === "level") {
       } else {
         translatorModelFields[field] = rawData[field];
       }
     }
   });
-  console.log({
-    userModelFields,
-    translatorModelFields
-  });
+  if (rawData["language"]) {
+    translatorModelFields["languages"] = languageFormatter(
+      rawData["language"],
+      rawData["level"]
+    );
+  }
+
   return {
     userModelFields,
     translatorModelFields
   };
 };
+
+function languageFormatter(langArr, lvlArr) {
+  // input =>  [ 'Oromo', 'Maninka', 'Fula' ], [ 'B2', 'B1', 'B1' ]
+  // output => [{language: "Oromo", level: "B2"}]
+  let formattedLanguages = [];
+  langArr.forEach((lang, ind) => {
+    let currentLang = {};
+    currentLang["language"] = lang;
+    currentLang["level"] = lvlArr[ind];
+    formattedLanguages.push(currentLang);
+  });
+
+  return formattedLanguages;
+}
